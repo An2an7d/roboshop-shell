3f0799eb28c2f99d319a423b5e3bc54facc32e4f
Anand <venkatanandn@gmail.com>
2023-08-23 11:03:18 +0530

    user modified

---
+++ cart.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip
+
+cd /app 
+
+unzip /tmp/cart.zip
+
+npm install 
+
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload
+
+systemctl enable cart 
+
+systemctl start cart
